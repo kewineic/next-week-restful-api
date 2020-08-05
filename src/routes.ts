bd17ba118b63f4e4cca7939399e2a9f4cd4d3c95
kewineic <kewin.ferreira@hotmail.com>
2020-08-05 20:35:15 -0300

    connections route created and debug migrations

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,10 @@
 import express from 'express';
-const routes = express.Router();
-
 import ClassesController from './controllers/ClassesController';
+import ConnectionsController from './controllers/ConnectionsController';
+
+const routes = express.Router();
 const classesController = new ClassesController();
+const connectionsController = new ConnectionsController;
 
 routes.get('/', (req, res) => {
     return res.json({message: 'Hello World'});
@@ -10,5 +12,8 @@ routes.get('/', (req, res) => {
 
 routes.get('/classes', classesController.index);
 routes.post('/classes', classesController.create);
+
+routes.get('/connections', connectionsController.index);
+routes.post('/connections', connectionsController.create);
 
 export default routes;
